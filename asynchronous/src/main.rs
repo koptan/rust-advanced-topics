a21ecdfa2937041806df67694996263d27f94d2b
Koptan <abdo.koptan@gmail.com>
2022-03-01 14:40:19 +0100

    removing commented code

--- asynchronous/src/main.rs
+++ asynchronous/src/main.rs
@@ -48,29 +48,6 @@ where
     }
 }
 
-// struct MyFuture {
-//     sleep: Pin<Box<Sleep>>,
-// }
-
-// impl MyFuture {
-//     fn new() -> Self {
-//         Self {
-//             sleep: Box::pin(tokio::time::sleep(Duration::from_secs(1))),
-//         }
-//     }
-// }
-
-// impl Future for MyFuture {
-//     type Output = ();
-
-//     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-//         println!("MyFuture::Pool()");
-//         self.sleep.as_mut().poll(cx)
-//     }
-// }
-
-
-
 #[tokio::main]
 async fn main() -> Result<(), tokio::io::Error> {
     let mut buf = vec![0u8; 128 * 1024];
